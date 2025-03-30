c2b82cd063db45c3183108b523cd71211a269223
AlbertFarkhutdinov <farhutdinovalbert@yandex.ru>
2025-03-30 19:31:35 +0400

    added docstring for src.backend

---
+++ src/backend/__init__.py
@@ -0,0 +1,14 @@
+"""
+A backend part of the Budget Analysis Service.
+
+It provides authentication, financial reporting,
+and expense tracking functionalities through various subpackages.
+
+Subpackages
+-----------
+- api: Provides FastAPI endpoints for interacting with the backend.
+- auth_app: Handles user authentication and authorization using AWS Cognito.
+- entries_app: Manages budget entries, transactions, and related data.
+- reports_app: Generates and manages financial reports.
+
+"""
